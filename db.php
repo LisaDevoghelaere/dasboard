6b4e38e3ef20832d6b975f114b0ac5c77bce5360
LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-06-23 14:46:55 +0200

    création structure
    mise en place des éléments de structure html et connection à la base de donnée

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php 
+define('DATABASE', 'dashboard');
+define('USER', 'root');
+define('PWD', '');
+define('HOST', 'dlocalhost');
+try{
+    $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD);
+
+}catch (PDOException $e){
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die;
+}
+
+
--- index.php
+++ index.php
@@ -1,11 +1,27 @@
+<?php
+require_once('db.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Mon Dashboard</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+<h1>Mon Dashboard</h1>
+<p>Suivit de consommation des ampoules</p>
+<table>
+    <tr>
+        <th>id</th>
+        <th>Marque</th>
+        <th>Puissance</th>
+        <th>Position</th>
+        <th>Etage</th>
+        <th>Date de changement</th>
+    </tr>
+</table>
     
 </body>
 </html>
