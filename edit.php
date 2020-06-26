df50d7644d26f651ba59ba9f6d154949afc7399c
LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-06-26 11:36:10 +0200

    ajout bootstrap

--- edit.php
+++ edit.php
@@ -89,55 +89,66 @@ if(count($_POST)>0){
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=MuseoModerno:wght@500;700&display=swap" rel="stylesheet"> 
     <title>ajouter-modifier</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<main>
+    <?php         require_once 'barnav.php'; ?>
+<main class="container">
     <?php
+    
+
+
         if(isset($_GET['id'])&& isset($_GET['edit'])){
             $txtTitle = "Modifier";
         }else{
                 $txtTitle= "Ajouter";
         }
     ?>
-    <h1><?=$txtTitle ?></h1>
-    <form action="" method="post">
-        <div>
-            <input type="text" name="marque" id="marque" placeholder="marque" value="<?=$marque; ?>">
-        </div>
-        <div>
-            <input type="text" name="puissance" id="puissance" placeholder="puissance" value="<?=$puissance; ?>">
-        </div>
-        <div>
-            <input type="text" name="position" id="position" placeholder="position" value="<?=$position; ?>">
-        </div>
-        <div>      
-            <input type="text" name="etage" id="etage" placeholder="etage" value="<?=$etage; ?>">
-        </div>
-        <div>
-            <input type="text" name="changement" id="changement" placeholder="date de changement" value="<?=$changement; ?>">
-        </div>
-        <div>
+    <h2><?=$txtTitle ?></h2>
+    <div class="bloc">
+        <form action="" method="post">
+            <div>
+                <input type="text" name="marque" id="marque" placeholder="marque" value="<?=$marque; ?>">
+            </div>
+            <div>
+                <input type="text" name="puissance" id="puissance" placeholder="puissance" value="<?=$puissance; ?>">
+            </div>
+            <div>
+                <input type="text" name="position" id="position" placeholder="position" value="<?=$position; ?>">
+            </div>
+            <div>      
+                <input type="text" name="etage" id="etage" placeholder="etage" value="<?=$etage; ?>">
+            </div>
+            <div>
+                <input type="date" name="changement" id="changement" class="except" placeholder="date de changement" value="<?=$changement; ?>">
+            </div>
+            <div>
+                <?php
+                    if(isset($_GET['id'])&& isset($_GET['edit'])){
+                        $txtButton = "Modifier";
+                    }else{
+                        $txtButton = "Ajouter";
+                    }
+                ?>
+                <button type="submit" class="btn btn-outline-primary"><?=$txtButton ?></button>
+            </div>
             <?php
                 if(isset($_GET['id'])&& isset($_GET['edit'])){
-                    $txtButton = "Modifier";
-                }else{
-                    $txtButton = "Ajouter";
-                }
             ?>
-            <button type="submit"><?=$txtButton ?></button>
-        </div>
-        <?php
-            if(isset($_GET['id'])&& isset($_GET['edit'])){
-        ?>
-            <input type="hidden" name="edit" value="1">
-            <input type="hidden" name="id" value="<?=$id; ?>">
-        <?php 
-            } 
-        ?>
-    </form>
+                <input type="hidden" name="edit" value="1">
+                <input type="hidden" name="id" value="<?=$id; ?>">
+            <?php 
+                } 
+            ?>
+        </form>
+    </div>
 </main>
-    
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>    
 </body>
 </html>
--- index.php
+++ index.php
@@ -5,17 +5,19 @@ require_once('db.php');
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=MuseoModerno:wght@500;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
         <title>Mon Dashboard</title>
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <header>
-            <h1>Mon Dashboard</h1>
-            <p>Suivit de consommation des ampoules</p>
-        </header>
-        <main>
-            <table>
+        <?php 
+            require_once 'barnav.php';
+        ?>
+        <main class="container">
+            <h2><p>Suivit de consommation des ampoules</p></h2>
+            <table class="table">
                 <tr><!-- Penser à mettre des id pour rendre le tableau accessible aux malvoyants-->
                     <th>id</th>
                     <th>Marque</th>
@@ -23,40 +25,44 @@ require_once('db.php');
                     <th>Position</th>
                     <th>Etage</th>
                     <th>Date de changement</th>
+                    <th>Bouton</th>
                 </tr>
                 <?php
-                //requête sql, préalablement testée (on u'tilise pas *)
-                $sql = "SELECT id, marque, puissance, position, etage, changement FROM ampoule";
-                //Récupération des données
-                $sth = $dbh->prepare($sql);  
-                $sth->execute();
-                   
-                $resultat = $sth->fetchAll(PDO::FETCH_ASSOC); 
-                     //On précise que l'on veut un tableau associatif
+                    //requête sql, préalablement testée (on u'tilise pas *)
+                    $sql = "SELECT id, marque, puissance, position, etage, changement FROM ampoule";
+                    //Récupération des données
+                    $sth = $dbh->prepare($sql);  
+                    $sth->execute();
+                    
+                    $resultat = $sth->fetchAll(PDO::FETCH_ASSOC); 
+                        //On précise que l'on veut un tableau associatif
 
-                $intlDateFormater = new IntlDateFormatter('fr_fr', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
-                    //gestion format de date français
-                //Pour afficher le tableau on fait un foreach qui parcoura le tableau
-                foreach($resultat as $ligne){
-                    echo '<tr>';
-                    echo'<td>' . $ligne['id'] . '</td> ';
-                    echo'<td>' . $ligne['marque'] . '</td> ';
-                    echo'<td>' . $ligne['puissance'] . '</td> ';
-                    echo'<td>' . $ligne['position'] . '</td> ';
-                    echo'<td>' . $ligne['etage'] . '</td> ';
-                    echo'<td>' . $ligne['changement'] . '</td> ';
-                    echo '<td>'.$intlDateFormater->format(strtotime($ligne['changement'])).'</td>';
-                    echo '<td><a href="edit.php?edit=1&id='.$ligne['id'].'">Modifier</a> <a href="delete.php?id='.$ligne['id'].'" >Supprimer</a></td>';
-                    echo '</tr>';
-                }
-            ?>
+                    $intlDateFormater = new IntlDateFormatter('fr_FR', IntlDateFormatter::SHORT, IntlDateFormatter::NONE);
+                        //gestion format de date français
+                    //Pour afficher le tableau on fait un foreach qui parcoura le tableau
+                    foreach($resultat as $ligne){
+                        echo '<tr>';
+                        echo'<td>' . $ligne['id'] . '</td> ';
+                        echo'<td>' . $ligne['marque'] . '</td> ';
+                        echo'<td>' . $ligne['puissance'] . '</td> ';
+                        echo'<td>' . $ligne['position'] . '</td> ';
+                        echo'<td>' . $ligne['etage'] . '</td> ';
+                        
+                        echo '<td>'.$intlDateFormater->format(strtotime($ligne['changement'])).'</td>';
+                        echo '<td><a class="btn btn-outline-success" href="edit.php?edit=1&id='.$ligne['id'].'">Modifier</a> <a class="btn btn-outline-danger" href="delete.php?id='.$ligne['id'].'" >Supprimer</a></td>';
+                        echo '</tr>';
+                    }
+                ?>
             </table>
             <?php
             if(count($resultat) === 0){
                 echo '<p>Aucune donnée disponible</p>';
             }
             ?>
-            <p><a href="edit.php"> Ajouter </a></p>
+            <p><a href="edit.php" class="btn btn-outline-primary"> Ajouter </a></p>
         </main>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     </body>
 </html>
