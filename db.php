66a7ef1f01ee16ca47f680b403e4871ea71cac0d
LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-06-26 13:45:57 +0200

    update id

--- db.php
+++ db.php
@@ -1,7 +1,12 @@
 <?php 
-define('DATABASE', 'dashboard');
+/*define('DATABASE', 'dashboard');
 define('USER', 'root');
 define('PWD', '');
+define('HOST', 'localhost');*/
+
+define('DATABASE', 'lisad_dashboard');
+define('USER', 'lisad');
+define('PWD', 'viHm9uwG+hfH2w==');
 define('HOST', 'localhost');
 try{
     $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD);
