29d4d18136ae7cf633ecaef3519e5233c5019757
LisaDevoghelaere <lisa.devoghelaere@gmail.com>
2020-06-29 15:32:14 +0200

    correction bug

--- db.php
+++ db.php
@@ -4,10 +4,7 @@ define('USER', 'root');
 define('PWD', '');
 define('HOST', 'localhost');
 
-/*define('DATABASE', 'lisad_dashboard');
-define('USER', 'lisad');
-define('PWD', 'viHm9uwG+hfH2w==');
-define('HOST', 'localhost');*/
+
 try{
     $dbh = new PDO('mysql:host='.HOST.';dbname='.DATABASE, USER, PWD);
 
